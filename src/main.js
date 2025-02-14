e8c84264f635322101cc44fcd79bac8161dd457e
Omar Mohamed El-Zanaty <omaromar123extra@gmail.com>
2025-02-14 20:51:17 +0200

    Import all functions at main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 //integrate all functions here
+import { playerMovement } from './playerMovement.js';
+import { initializeGame } from './initializeTheGame.js';
+import { calculateScore } from './calcScore.js';
+import { checksGameOver } from './gameResult.js';
+
+//integrate all functions here
+initializeGame();
+playerMovement();
+calculateScore(100);
+checksGameOver();
